b48f72e11a2e5f25eeb9392ddf9e43bf4b10e0fc
Thiago Felicio Alves <thiago.alves@phidelis.com.br>
2022-08-22 13:34:15 -0300

    Adicionar arquivos de projeto.

---
+++ API/Controllers/VersaoController.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace API.Controllers
+{
+    /// <summary>
+    /// Versao
+    /// </summary>
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    [RoutePrefix("api/Versao")]
+    public class VersaoController : ApiController
+    {
+        /// <summary>
+        /// Obter
+        /// </summary>        
+        /// <param name="requisicao"></param>
+        /// <returns>RespostaAPI</returns>
+        /// 
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        [Route("Obter")]
+        public string Obter()
+        {
+         
+            return "opa";
+        }
+
+    }
+}
